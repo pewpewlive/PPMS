d8fe8e3ac6b8108db01d757982f4a5000afbe83f
Dominykas M <26067386+Tasty-Kiwi@users.noreply.github.com>
2023-05-31 21:34:44 +0300

    start rewriting

---
+++ src/MainMenu.tsx
@@ -0,0 +1,133 @@
+import {
+  makeStyles,
+  shorthands,
+  Button,
+  Display,
+  Card,
+  CardFooter,
+  CardHeader,
+  CardPreview,
+  Body1,
+  Caption1,
+} from "@fluentui/react-components"
+
+import { AddRegular, ArrowImportRegular } from "@fluentui/react-icons"
+
+const useStyles = makeStyles({
+  container: {
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+  },
+  textContainer: {
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "strech",
+  },
+  buttonGroup: {
+    marginTop: "1rem",
+    marginBottom: "1rem",
+    columnGap: "15px",
+    display: "flex",
+  },
+  cards: {
+    display: "flex",
+    flexWrap: "wrap",
+    columnGap: "15px",
+    marginTop: "1rem",
+    marginBottom: "1rem"
+  },
+})
+
+function MainMenu() {
+  const styles = useStyles()
+  return (
+    <div className={styles.container}>
+      <Display>PewPew Mesh Studio</Display>
+      <div className={styles.buttonGroup}>
+        <Button appearance="primary" icon={<AddRegular />}>
+          New
+        </Button>
+        <Button appearance="primary" icon={<ArrowImportRegular />}>
+          Import
+        </Button>
+      </div>
+      <div className={styles.cards}>
+        <Card>
+          <CardHeader header={<Body1>Sphere of mass destruction</Body1>} />
+          <CardPreview>
+            <img src="https://placehold.co/300x200/" />
+          </CardPreview>
+          <CardFooter className={styles.textContainer}>
+            <Caption1 align="end">June 1st, 2023</Caption1>
+          </CardFooter>
+        </Card>
+        <Card>
+          <CardHeader header={<Body1>Sphere of mass destruction</Body1>} />
+          <CardPreview>
+            <img src="https://placehold.co/300x200/" />
+          </CardPreview>
+          <CardFooter className={styles.textContainer}>
+            <Caption1 align="end">June 1st, 2023</Caption1>
+          </CardFooter>
+        </Card>
+        <Card>
+          <CardHeader header={<Body1>Sphere of mass destruction</Body1>} />
+          <CardPreview>
+            <img src="https://placehold.co/300x200/" />
+          </CardPreview>
+          <CardFooter className={styles.textContainer}>
+            <Caption1 align="end">June 1st, 2023</Caption1>
+          </CardFooter>
+        </Card>
+        <Card>
+          <CardHeader header={<Body1>Sphere of mass destruction</Body1>} />
+          <CardPreview>
+            <img src="https://placehold.co/300x200/" />
+          </CardPreview>
+          <CardFooter className={styles.textContainer}>
+            <Caption1 align="end">June 1st, 2023</Caption1>
+          </CardFooter>
+        </Card>
+        <Card>
+          <CardHeader header={<Body1>Sphere of mass destruction</Body1>} />
+          <CardPreview>
+            <img src="https://placehold.co/300x200/" />
+          </CardPreview>
+          <CardFooter className={styles.textContainer}>
+            <Caption1 align="end">June 1st, 2023</Caption1>
+          </CardFooter>
+        </Card>
+        <Card>
+          <CardHeader header={<Body1>Sphere of mass destruction</Body1>} />
+          <CardPreview>
+            <img src="https://placehold.co/300x200/" />
+          </CardPreview>
+          <CardFooter className={styles.textContainer}>
+            <Caption1 align="end">June 1st, 2023</Caption1>
+          </CardFooter>
+        </Card>
+        <Card>
+          <CardHeader header={<Body1>Sphere of mass destruction</Body1>} />
+          <CardPreview>
+            <img src="https://placehold.co/300x200/" />
+          </CardPreview>
+          <CardFooter className={styles.textContainer}>
+            <Caption1 align="end">June 1st, 2023</Caption1>
+          </CardFooter>
+        </Card>
+        <Card>
+          <CardHeader header={<Body1>Sphere of mass destruction</Body1>} />
+          <CardPreview>
+            <img src="https://placehold.co/300x200/" />
+          </CardPreview>
+          <CardFooter className={styles.textContainer}>
+            <Caption1 align="end">June 1st, 2023</Caption1>
+          </CardFooter>
+        </Card>
+      </div>
+    </div>
+  )
+}
+
+export default MainMenu
